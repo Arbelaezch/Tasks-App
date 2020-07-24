99421adefab0e326d12fe50467253b6f18fd1a96
Arbelaezch <arbelaezch@gmail.com>
2020-07-23 19:58:16 -0600

    Guide list added

--- app/src/main/java/com/example/tasks/MainActivity.java
+++ app/src/main/java/com/example/tasks/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.tasks.guides.GuideMainActivity;
 import com.example.tasks.tasks.tasksMainActivity;
@@ -13,6 +14,7 @@ import com.example.tasks.tasks.tasksMainActivity;
 public class MainActivity extends AppCompatActivity {
     private Button guideButton;
     private Button taskButton;
+
 
 
     @Override
--- app/src/main/java/com/example/tasks/guides/GuideMainActivity.java
+++ app/src/main/java/com/example/tasks/guides/GuideMainActivity.java
@@ -3,14 +3,26 @@ package com.example.tasks.guides;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.tasks.R;
 
 public class GuideMainActivity extends AppCompatActivity {
+    private ListView listView;
+
+    String[] nameArray = {"Walrus","Pig","Sheep","Rabbit","Snake","Spider" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_main);
+
+        guide_adapter whatever = new guide_adapter(this, nameArray);
+
+        // Reference to the listView from activity_main.xml using its id.
+        listView = findViewById(R.id.guides_list);
+        listView.setAdapter(whatever);
+
     }
 }
